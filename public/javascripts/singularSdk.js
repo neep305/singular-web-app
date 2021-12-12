ca990a9304f2b2bbe29f54827d8cc8fba4df8621
Jason Nam <neep305@gmail.com>
2021-12-13 01:25:59 +0900

    add web to app event

---
+++ public/javascripts/singularSdk.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const initSingularSdk = () => {
+    const apiKey="jaredinternaltestaccount_5c7f106a";
+    const secretKey="65c36d10fff97b67e34b6c36714c4f52";
+    const productId="com.herokuapp.singular-web-app";
+    const config = new SingularConfig(apiKey, secretKey, productId);
+    singularSdk.init(config);
+}
+
+const openApp = () => {
+    console.log('executed openApp()');
+    // https://jared.sng.link/Asyhj/eh31?_ios_dl=jasonapp%3A%2F%2F&_android_dl=jasonapp
+    singularSdk.openApp("https://jared.sng.link/Asyhj/eh31", "jasonapp://", "passthrough_test", "jasonapp://");
+}
