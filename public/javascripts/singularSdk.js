e8270c0ac0276edb8dd43680df0978cf79ab621b
Jason Nam <neep305@gmail.com>
2022-02-16 11:58:39 +0900

    change openApp url

--- public/javascripts/singularSdk.js
+++ public/javascripts/singularSdk.js
@@ -19,7 +19,7 @@ const sendRevenue = (eventName, currency, amount, args) => {
 
 const openApp = () => {
     // https://jared.sng.link/Asyhj/eh31?_ios_dl=jasonapp%3A%2F%2F&_android_dl=jasonapp
-    singularSdk.openApp("https://se.sng.link/Dsyhj/zzku", "jasonapp://dl", "passthrough_value", "jasonapp://ddl");
+    singularSdk.openApp("https://jared.sng.link/Asyhj/eh31?_ios_dl=jasonapp%3A%2F%2F&_android_dl=jasonapp", "", "", "");
 }
 
 const buildWebToAppLink = () => {
